65e00ba9d15fced159bb34eab1565a89b6beb526
Samuel Corradi <samuel.corradi2@dxc.com>
2026-01-10 14:03:56 -0300

    Create ProductionRuleNode role node

--- src/pyrsing/nodes.py
+++ src/pyrsing/nodes.py
@@ -17,7 +17,48 @@ class AnyNode(ASTNode):
             print(inp_char, '.')
         except StopIteration:
             raise NotMatchException(f"Expected any character in grammar, got end of input at position '{input.get_pos()}'.")
-        return
+
+class ProductionRuleNode(ASTNode):
+    """
+    Production rule com nome: <rule_name>
+    """
+    def __init__(self, name: str):
+        super().__init__()
+        self.name = name
+
+    @staticmethod
+    def parse_rule_name(rule_str:str)->tuple[str,str]:
+        """
+        Extracts the token name and alias (if any) from a rule.
+        Returns a tuple (name, alias).
+        Throws an exception if the syntax is incorrect.
+        """
+        mask = r'<([^>:]+)(:[^>]+)?>'
+        match = re.match(mask, rule_str)
+        if not match:
+            raise Exception(f"Syntax error on rule '{rule_str}'.")
+        token_name = match.group(1)
+        alias = match.group(2)
+        return token_name, alias
+
+    def parse(self, input: Input):
+        results = []
+        for item in self.children:
+            if isinstance(item, ASTNode):
+                res = item.parse(input)
+                results.append(res)
+        if self.is_negation:
+            raise NotMatchException("Negation matched when it should not have.")
+        # senao, eh uma sequencia/grupo
+        return RuleNode(self.name, results)
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}:{self.name}>" \
+            + f"{' OPTIONAL' if self.is_optional else ''}" \
+            + f"{' REPEATER' if self.is_repeat else ''}" \
+            + f"{' NEGATION' if self.is_negation else ''}"
+
+
 
 class SequenceNode(ASTNode):
     """
