968cf58097dda56a583bf03f5e5dde0a7781e9c4
Samuel Corradi <samuelcorradi@gmail.com>
2026-02-14 20:45:41 +0000

    Refactor RepetitionNode's _parse_element method for clarity and improved comments

--- src/pyrsing/nodes.py
+++ src/pyrsing/nodes.py
@@ -136,50 +136,42 @@ class RepetitionNode(ASTNode):
         self.children = [child]
         self.min_times = min_times
         self.max_times = max_times
-        self.is_negation = False # Repetition itself isn't negation, though child can be
+        self.is_negation = False # repetition itself isn't negation, though child can be
 
     def _parse_element(self, input: Input):
         num_rep = 0
         result = []
         child = self.children[0]
-        
         while True:
             input_pos = input.get_pos()
             try:
-                # Parse the child
                 item = child._parse(input)
-                
-                # If item is None (optional returned None), we stop
+                # if item is None (optional returned None), we stop
                 if item is None and child.is_optional:
                     break
-
                 if isinstance(item, list):
                     result.extend(item)
                 else:
                     result.append(item)
-                
                 num_rep += 1
-                
-                # PROTECTION: If a match occurred but input didn't advance (empty match),
-                # we must break to avoid infinite loop -> (foo)* where foo matches empty
+                # PROTECTION
+                # if a match occurred but input
+                # didn't advance (empty match),
+                # we must break to avoid infinite loop -> (foo)*
+                # where foo matches empty
                 if input.get_pos() == input_pos:
                      break
-
-                # Check max repetition
+                # check max repetition
                 if self.max_times is not None and num_rep == self.max_times:
                     break
-                    
             except Exception as e:
-                # In case of mismatch on next attempt
+                # in case of mismatch on next attempt
                 input.set_pos(input_pos)
-                
-                # If we haven't reached min_times yet, it's an error
+                # if we haven't reached min_times yet, it's an error
                 if num_rep < self.min_times:
                      raise NotMatchException(f"Did not achieve the expected number of repetitions. {self.min_times} repetitions were expected, but only {num_rep} were performed.")
-                
-                # Otherwise, we are done with the loop
+                # otherwise, we are done with the loop
                 break
-        
         return TokenSequence(name=None, children=result)
 
     def __str__(self):
